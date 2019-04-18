83d0aa14b69a9dec1dd0fecdf076fe153df7ff63
j-buss <jeremy.f.buss@gmail.com>
2019-04-17 22:08:24 -0500

    updated function

--- list_file.py
+++ list_file.py
@@ -1,7 +1,12 @@
 from google.cloud import storage
-client = storage.Client()
-# https://console.cloud.google.com/storage/browser/[bucket-id]/
-bucket = client.get_bucket('landing-009')
-# Then do other things...
-blob = bucket.get_blob('all_staff_report/2017_2018/AllStaffReportPublic__04152019_194414.csv')
-print(blob.download_as_string())
+
+def set_variables(bucket_name, blob_name):
+    client = storage.Client()
+    # https://console.cloud.google.com/storage/browser/[bucket-id]/
+    bucket = client.get_bucket(bucket_name)
+    # Then do other things...
+    blob = bucket.get_blob(blob_name)
+    print(blob.download_as_string())
+
+if __name__ == "__main__":
+    set_variables()
