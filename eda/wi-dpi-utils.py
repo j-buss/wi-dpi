aba8315512d829d6a75c3d21b55b18036a227821
j-buss <jeremy.f.buss@gmail.com>
2019-05-05 18:53:57 -0500

    adding util file

---
+++ eda/wi-dpi-utils.py
@@ -0,0 +1,52 @@
+file_dict = [
+    {'old_name':"all_staff_report/temp/AllStaff_Open_Files/95staff.txt",/
+		'new_name':"all_staff_report/1995_1996/95staff.txt",'landing_tablename':"1995",'file_type':"fixed"},
+    {'old_name':"all_staff_report/temp/AllStaff_Open_Files/96staff.txt",/
+		'new_name':"all_staff_report/1996_1997/96staff.txt",'landing_tablename':"1996",'file_type':"fixed"},
+    {'old_name':"all_staff_report/temp/AllStaff_Open_Files/97staff.txt",/
+		'new_name':"all_staff_report/1997_1998/97staff.txt",'landing_tablename':"1997",'file_type':"fixed"},
+    {'old_name':"all_staff_report/temp/AllStaff_Open_Files/98staff.txt",/
+		'new_name':"all_staff_report/1998_1999/98staff.txt",'landing_tablename':"1998",'file_type':"fixed"},
+    {'old_name':"all_staff_report/temp/AllStaff_Open_Files/99STAFF.DAT",/
+		'new_name':"all_staff_report/1999_2000/99STAFF.DAT",'landing_tablename':"1999",'file_type':"fixed"},
+    
+    
+    {'old_name':"all_staff_report/temp/AllStaff_Open_Files/00staff.dat",/
+		'new_name':"all_staff_report/2000_2001/00staff.dat",'landing_tablename':"2000"},
+    {'old_name':"all_staff_report/temp/AllStaff_Open_Files/01staff.dat",/
+		'new_name':"all_staff_report/2001_2002/01staff.dat",'landing_tablename':"2001"},
+    {'old_name':"all_staff_report/temp/AllStaff_Open_Files/02staff.txt",/
+		'new_name':"all_staff_report/2002_2003/02staff.txt",'landing_tablename':"2002"},
+    {'old_name':"all_staff_report/temp/AllStaff_Open_Files/03staff.txt",/
+		'new_name':"all_staff_report/2003_2004/03staff.txt",'landing_tablename':"2003"},
+    {'old_name':"all_staff_report/temp/AllStaff_Open_Files/04staff.dat",/
+		'new_name':"all_staff_report/2004_2005/04staff.dat",'landing_tablename':"2004"},
+                                                                                                                                                 
+    {'old_name':"all_staff_report/temp/AllStaff_Open_Files/05staff.txt",/
+		'new_name':"all_staff_report/2005_2006/05staff.txt",'landing_tablename':"2005"},
+    {'old_name':"all_staff_report/temp/AllStaff_Open_Files/06staff.txt",/
+		'new_name':"all_staff_report/2006_2007/06staff.txt",'landing_tablename':"2006"},
+    {'old_name':"all_staff_report/temp/AllStaff_Open_Files/07staff.txt",/
+		'new_name':"all_staff_report/2007_2008/07staff.txt",'landing_tablename':"2007"},
+    {'old_name':"all_staff_report/temp/AllStaff_Open_Files/08STAFF.TXT",/
+		'new_name':"all_staff_report/2008_2009/08STAFF.TXT",'landing_tablename':"2008"},
+    {'old_name':"all_staff_report/temp/AllStaff_Open_Files/09STAFF.TXT",/
+		'new_name':"all_staff_report/2009_2010/09STAFF.TXT",'landing_tablename':"2009"},
+                                                                                                                                                 
+    {'old_name':"all_staff_report/temp/AllStaff_Open_Files/10STAFF.TXT",/
+		'new_name':"all_staff_report/2010_2011/10STAFF.TXT",'landing_tablename':"2010"},
+    {'old_name':"all_staff_report/temp/AllStaff_Open_Files/11STAFF.txt",/
+		'new_name':"all_staff_report/2011_2012/11STAFF.txt",'landing_tablename':"2011"},
+    {'old_name':"all_staff_report/temp/AllStaff_Open_Files/12STAFF.txt",/
+		'new_name':"all_staff_report/2012_2013/12STAFF.txt",'landing_tablename':"2012"},
+    {'old_name':"all_staff_report/temp/AllStaff_Open_Files/13staff.txt",/
+		'new_name':"all_staff_report/2013_2014/13staff.txt",'landing_tablename':"2013"},
+    {'old_name':"all_staff_report/temp/AllStaff_Open_Files/14staff.txt",/
+		'new_name':"all_staff_report/2014_2015/14staff.txt",'landing_tablename':"2014"},
+    
+    {'old_name':"all_staff_report/temp/AllStaff_Open_Files/2015.csv",/
+		'new_name':"all_staff_report/2015_2016/2015.csv",'landing_tablename':"2015"},
+    {'old_name':"all_staff_report/temp/AllStaff_Open_Files/2016.csv",/
+		'new_name':"all_staff_report/2016_2017/2016.csv",'landing_tablename':"2016"}
+      
+]
