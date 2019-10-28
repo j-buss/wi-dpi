6dc755198ce40859ccf09aa9db3f422fd104578c
j-buss <jeremy.f.buss@gmail.com>
2019-10-28 17:36:20 -0500

    updated getAllStaffReport.py

--- pipeline/python/getAllStaffReport.py
+++ pipeline/python/getAllStaffReport.py
@@ -5,21 +5,14 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
 #logging.disable(logging.CRITICAL)
 
+zipFileObject = collections.namedtuple("zipFileObject", "year outputfile url")
+
 def get_staff_report_new():
+
 
     baseURL = "https://publicstaffreports.dpi.wi.gov/PubStaffReport/Public/PublicReport/AllStaffReportDownload"
 
-    ## ?selectedYear=2019&
-    #hiringLea=&
-    #workingLea=&
-    #position=&
-    #assignmentArea=&
-    #licenseType=&
-    #firstNameContains=&
-    #lastNameContains=&
-    #criteria=Filter%20Criteria:%20Selected%20Year:%202018%20-%202019;%20Selected%20Hiring%20Agencies:%20none;%20Selected%20Working%20Agencies:%20none;%20Selected%20Assignment%20Position:%20--%20All%20Positions%20--;%20Selected%20Assignment%20Area:%20--%20All%20Areas%20--;%20Selected%20First%20Name:%20none;%20Selected%20Last%20Name:%20none;%20Selected%20License%20Type:%20--%20All%20License%20Types%20--
-
-    params = collections.OrderedDict([('selectedYear', 2019), ('lastNameContains', 'Jackson')])
+    params = collections.OrderedDict([('selectedYear', 2019)])
 
     r = requests.get(baseURL, params=params)
     r.raise_for_status()
@@ -33,23 +26,32 @@ def get_staff_report_new():
         file_download.write(chunk)
     file_download.close()
 
-def get_staff_report_zip():
+def get_staff_report_zip(zipFileObject):
     fileList = [
-        ('2013', '2013.zip', 'https://dpi.wi.gov/sites/default/files/imce/cst/exe/13Staff.zip'),
-        ('2014', '2014.zip', 'https://dpi.wi.gov/sites/default/files/imce/cst/exe/14staff.zip'),
-        ('2015', '2015.zip', 'https://dpi.wi.gov/sites/default/files/imce/cst/exe/AllStaff2015rev10-31-2016.zip'),
-        ('2016', '2016.zip', 'https://dpi.wi.gov/sites/default/files/imce/cst/exe/AllStaff2016rev11-01-16.zip')
+        ('2013', '/home/jeremyfbuss/wi-dpi-analysis/data/raw/2013.zip', 'https://dpi.wi.gov/sites/default/files/imce/cst/exe/13Staff.zip'),
+        ('2014', '/home/jeremyfbuss/wi-dpi-analysis/data/raw/2014.zip', 'https://dpi.wi.gov/sites/default/files/imce/cst/exe/14staff.zip'),
+        ('2015', '/home/jeremyfbuss/wi-dpi-analysis/data/raw/2015.zip', 'https://dpi.wi.gov/sites/default/files/imce/cst/exe/AllStaff2015rev10-31-2016.zip'),
+        ('2016', '/home/jeremyfbuss/wi-dpi-analysis/data/raw/2016.zip', 'https://dpi.wi.gov/sites/default/files/imce/cst/exe/AllStaff2016rev11-01-16.zip')
     ]
-    for f in fileList:
-        logging.debug(f[1])
+    logging.debug(zipFileObject.url)
 
-        r = requests.get(f[1])
+   r = requests.get(f.url)
         r.raise_for_status()
 
-        file_download = open(f[0], 'wb')
+        file_download = open(f.outputfile, 'wb')
         for chunk in r.iter_content(100000):
             file_download.write(chunk)
         file_download.close()
 
 if __name__ == "__main__":
-   get_staff_report_zip()
+
+
+#    zipFileObject = collections.namedtuple("zipFileObject","year outputfile url")
+    zipFileList = []
+    zipFileList.append(zipFileObject(year="2013",
+                                     outputfile="/home/jeremyfbuss/wi-dpi-analysis/data/raw/2013.zip",
+                                     url="https://dpi.wi.gov/sites/default/files/imce/cst/exe/13Staff.zip"))
+    for f in zipFileList:
+        logging.debug(f)
+       # get_staff_report_zip(f)
+   #get_staff_report_new()
