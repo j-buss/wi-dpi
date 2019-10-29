537eca547bf4c16e8da9714a92b605f61e6031ca
j-buss <jeremy.f.buss@gmail.com>
2019-10-29 06:00:07 -0500

    added common download and save function to getAllStaffReport.py

--- pipeline/python/getAllStaffReport.py
+++ pipeline/python/getAllStaffReport.py
@@ -3,55 +3,64 @@ import collections
 import logging
 
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
-#logging.disable(logging.CRITICAL)
 
-zipFileObject = collections.namedtuple("zipFileObject", "year outputfile url")
+
+# logging.disable(logging.CRITICAL)
+
 
 def get_staff_report_new():
-
-
-    baseURL = "https://publicstaffreports.dpi.wi.gov/PubStaffReport/Public/PublicReport/AllStaffReportDownload"
+    base_url = "https://publicstaffreports.dpi.wi.gov/PubStaffReport/Public/PublicReport/AllStaffReportDownload"
 
     params = collections.OrderedDict([('selectedYear', 2019)])
 
-    r = requests.get(baseURL, params=params)
-    r.raise_for_status()
+    req = requests.get(base_url, params=params)
+    req.raise_for_status()
 
-    #weatherData = json.loads(r.text)
+    # weatherData = json.loads(r.text)
 
     # Save the data to disk
-    #fileDownload = open(os.path.join(directory, os.path.basename(comicUrl)), 'wb')
+    # fileDownload = open(os.path.join(directory, os.path.basename(comicUrl)), 'wb')
     file_download = open('test_download.xlsx', 'wb')
     for chunk in r.iter_content(100000):
         file_download.write(chunk)
     file_download.close()
 
-def get_staff_report_zip(zipFileObject):
-    fileList = [
-        ('2013', '/home/jeremyfbuss/wi-dpi-analysis/data/raw/2013.zip', 'https://dpi.wi.gov/sites/default/files/imce/cst/exe/13Staff.zip'),
-        ('2014', '/home/jeremyfbuss/wi-dpi-analysis/data/raw/2014.zip', 'https://dpi.wi.gov/sites/default/files/imce/cst/exe/14staff.zip'),
-        ('2015', '/home/jeremyfbuss/wi-dpi-analysis/data/raw/2015.zip', 'https://dpi.wi.gov/sites/default/files/imce/cst/exe/AllStaff2015rev10-31-2016.zip'),
-        ('2016', '/home/jeremyfbuss/wi-dpi-analysis/data/raw/2016.zip', 'https://dpi.wi.gov/sites/default/files/imce/cst/exe/AllStaff2016rev11-01-16.zip')
-    ]
-    logging.debug(zipFileObject.url)
 
-   r = requests.get(f.url)
-        r.raise_for_status()
+def download_and_save(input_url_file_tuple):
+    logging.debug(input_url_file_tuple.url)
+    if input_url_file_tuple.params == "":
+        res = requests.get(input_url_file_tuple.url)
+    else:
+        res = requests.get(input_url_file_tuple.url, params=url_file_tuple.params)
 
-        file_download = open(f.outputfile, 'wb')
-        for chunk in r.iter_content(100000):
-            file_download.write(chunk)
-        file_download.close()
+    res.raise_for_status()
+
+    file_download = open(input_url_file_tuple.output_file, 'wb')
+    for chunk in res.iter_content(100000):
+        file_download.write(chunk)
+    file_download.close()
+
 
 if __name__ == "__main__":
 
+    url_file_tuple = collections.namedtuple("urlFileTuple", "name output_file url params")
 
-#    zipFileObject = collections.namedtuple("zipFileObject","year outputfile url")
-    zipFileList = []
-    zipFileList.append(zipFileObject(year="2013",
-                                     outputfile="/home/jeremyfbuss/wi-dpi-analysis/data/raw/2013.zip",
-                                     url="https://dpi.wi.gov/sites/default/files/imce/cst/exe/13Staff.zip"))
-    for f in zipFileList:
+    file_list = [url_file_tuple(name="2013",
+                                output_file="/home/jeremyfbuss/wi-dpi-analysis/data/raw/2013.zip",
+                                url="https://dpi.wi.gov/sites/default/files/imce/cst/exe/13Staff.zip",
+                                params=""),
+                 url_file_tuple(name="2014",
+                                output_file="/home/jeremyfbuss/wi-dpi-analysis/data/raw/2014.zip",
+                                url="https://dpi.wi.gov/sites/default/files/imce/cst/exe/14staff.zip",
+                                params=""),
+                 url_file_tuple(name="2015",
+                                output_file="/home/jeremyfbuss/wi-dpi-analysis/data/raw/2015.zip",
+                                url="https://dpi.wi.gov/sites/default/files/imce/cst/exe/AllStaff2015rev10-31-2016.zip",
+                                params=""),
+                 url_file_tuple(name="2016",
+                                output_file="/home/jeremyfbuss/wi-dpi-analysis/data/raw/2016.zip",
+                                url="https://dpi.wi.gov/sites/default/files/imce/cst/exe/AllStaff2016rev11-01-16.zip",
+                                params="")]
+    for f in file_list:
         logging.debug(f)
-       # get_staff_report_zip(f)
-   #get_staff_report_new()
+        download_and_save(f)
