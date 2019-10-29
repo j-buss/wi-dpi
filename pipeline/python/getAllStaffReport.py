2584b362a15539f2d5fabafbc8d71f70a72dafec
j-buss <jeremy.f.buss@gmail.com>
2019-10-29 17:14:37 -0500

    added in the url_file_tuple

--- pipeline/python/getAllStaffReport.py
+++ pipeline/python/getAllStaffReport.py
@@ -8,30 +8,12 @@ logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - 
 # logging.disable(logging.CRITICAL)
 
 
-def get_staff_report_new():
-    base_url = "https://publicstaffreports.dpi.wi.gov/PubStaffReport/Public/PublicReport/AllStaffReportDownload"
-
-    params = collections.OrderedDict([('selectedYear', 2019)])
-
-    req = requests.get(base_url, params=params)
-    req.raise_for_status()
-
-    # weatherData = json.loads(r.text)
-
-    # Save the data to disk
-    # fileDownload = open(os.path.join(directory, os.path.basename(comicUrl)), 'wb')
-    file_download = open('test_download.xlsx', 'wb')
-    for chunk in r.iter_content(100000):
-        file_download.write(chunk)
-    file_download.close()
-
-
 def download_and_save(input_url_file_tuple):
     logging.debug(input_url_file_tuple.url)
     if input_url_file_tuple.params == "":
         res = requests.get(input_url_file_tuple.url)
     else:
-        res = requests.get(input_url_file_tuple.url, params=url_file_tuple.params)
+        res = requests.get(input_url_file_tuple.url, params=input_url_file_tuple.params)
 
     res.raise_for_status()
 
@@ -60,7 +42,24 @@ if __name__ == "__main__":
                  url_file_tuple(name="2016",
                                 output_file="/home/jeremyfbuss/wi-dpi-analysis/data/raw/2016.zip",
                                 url="https://dpi.wi.gov/sites/default/files/imce/cst/exe/AllStaff2016rev11-01-16.zip",
-                                params="")]
-    for f in file_list:
+                                params=""),
+                 url_file_tuple(name="2017",
+                                output_file="/home/jeremyfbuss/wi-dpi-analysis/data/raw/2017.xlsx",
+                                url="https://publicstaffreports.dpi.wi.gov/PubStaffReport/Public/PublicReport/AllStaffReportDownload",
+                                params=collections.OrderedDict([('selectedYear', 2017)])),
+                 url_file_tuple(name="2018",
+                                output_file="/home/jeremyfbuss/wi-dpi-analysis/data/raw/2018.xlsx",
+                                url="https://publicstaffreports.dpi.wi.gov/PubStaffReport/Public/PublicReport/AllStaffReportDownload",
+                                params=collections.OrderedDict([('selectedYear', 2018)])),
+                 url_file_tuple(name="2019",
+                                output_file="/home/jeremyfbuss/wi-dpi-analysis/data/raw/2019.xlsx",
+                                url="https://publicstaffreports.dpi.wi.gov/PubStaffReport/Public/PublicReport/AllStaffReportDownload",
+                                params=collections.OrderedDict([('selectedYear', 2019)]))
+                 ]
+    # for f in file_list: # Do all of the files
+    # To run all the files use: file_list
+    #      to run subset use: [file_list[-1]]
+    for f in [file_list[-1]]:
         logging.debug(f)
         download_and_save(f)
+    # ADD a check to see if the file is already downloaded
