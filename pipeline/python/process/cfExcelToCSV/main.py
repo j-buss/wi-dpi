6d87a2e8985522b9305cdb4e1e55b9e1e438a90d
j-buss <jeremy.f.buss@gmail.com>
2019-11-08 06:02:39 -0600

    cloud function working for testing; need to access project id from inside function (data or context?)

--- pipeline/python/process/cfExcelToCSV/main.py
+++ pipeline/python/process/cfExcelToCSV/main.py
@@ -1,8 +1,12 @@
 from google.cloud import storage
 import openpyxl
 import pandas as pd
+import gcsfs
 import os
+import logging
 
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+#logging.disable(logging.CRITICAL)
 
 def excel_to_csv(data, context):
     # Config
@@ -17,34 +21,10 @@ def excel_to_csv(data, context):
         source_bucket = storage_client.get_bucket(source_bucket_name)
         source_blob = source_bucket.blob(source_blob_name)
 
-        data_xls = pd.read_excel(source_blob, 'Sheet1', index_col=None)
-        data_xls.to_csv('your_csv.csv', encoding='utf-8')
-
-        wb = openpyxl.load_workbook(source_blob)
-        # wb = openpyxl.load_workbook("gs://landing-008/Test_Spreadsheet.xlsx")
-        # Get source blob in usable state
-
-        # Cycle through spreadsheet - convert one tab at a time
-        for sheet in wb.sheetnames:
-
-            # For each Sheet
-            print("Converting sheet: " + sheet)
-            csv_file_name = source_blob_basename + "_" + sheet + ".csv"
-            print("Creating file: " + csv_file_name)
-            source_sheet = wb.get_sheet_by_name(sheet)
-            sheet_max_col = source_sheet.max_column
-            sheet_max_row = source_sheet.max_row
-
-            # Create Target File
-            # target_file = open(csv_file_name, 'w')
-            # target_writer = csv.writer(target_file)
-
-            # for row in range(1, sheet_max_row + 1):
-            #     rowArray = []
-            # for column in range(1, sheet_max_col):
-            # rowArray.append(source_sheet.cell(row=row, column=column).value)
-
-    # target_writer.writerow(rowArray)
-    # target_blob = target_bucket.blob(contentfilename)
-    # target_blob.upload_from_string(contentfile)
-# targetFile.close()
+        logging.debug(source_blob_name)
+        fs = gcsfs.GCSFileSystem(project='wi-dpi-010')
+        # fs = gcsfs.GCSFileSystem()
+        with fs.open(os.path.join(source_bucket_name, source_blob_name)) as f:
+            df = pd.read_excel(f)
+            with fs.open(os.path.join(source_bucket_name, source_blob_basename + ".csv"), "w") as out:
+                df.to_csv(out, encoding='utf-8', index=False)
