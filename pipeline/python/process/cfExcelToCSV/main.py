c64680fcd7c9ca7b9e86027965b1c55b4d908e86
j-buss <jeremy.f.buss@gmail.com>
2019-11-08 16:18:10 -0600

    updated cloud function excel_to_csv

--- pipeline/python/process/cfExcelToCSV/main.py
+++ pipeline/python/process/cfExcelToCSV/main.py
@@ -1,29 +1,25 @@
 from google.cloud import storage
-import openpyxl
 import pandas as pd
 import gcsfs
 import os
 import logging
 
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
-#logging.disable(logging.CRITICAL)
+logging.disable(logging.CRITICAL)
+
 
 def excel_to_csv(data, context):
     # Config
     source_bucket_name = data['bucket']
     source_blob_name = data['name']
     source_blob_basename, source_blob_ext = os.path.splitext(os.path.basename(source_blob_name))
+    project_id = os.environ.get('GCP_PROJECT')
 
     if source_blob_ext == ".xlsx":
         print("Converting workbook: " + source_blob_name)
-        # Get source blob in usable state
-        storage_client = storage.Client()
-        source_bucket = storage_client.get_bucket(source_bucket_name)
-        source_blob = source_bucket.blob(source_blob_name)
 
         logging.debug(source_blob_name)
-        fs = gcsfs.GCSFileSystem(project='wi-dpi-010')
-        # fs = gcsfs.GCSFileSystem()
+        fs = gcsfs.GCSFileSystem(project=project_id)
         with fs.open(os.path.join(source_bucket_name, source_blob_name)) as f:
             df = pd.read_excel(f)
             with fs.open(os.path.join(source_bucket_name, source_blob_basename + ".csv"), "w") as out:
