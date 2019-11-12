7bf1c3a6ad095ca2ec1abe2288249b93d484c61e
j-buss <jeremy.f.buss@gmail.com>
2019-11-12 06:05:48 -0600

    adding staging logic

--- pipeline/python/process/bqLoad/loadBQStaging.py
+++ pipeline/python/process/bqLoad/loadBQStaging.py
@@ -15,7 +15,7 @@ logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - 
 
 def clean_column_headers(columns):
     return columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '').str.replace(
-        '.', '_').str.replace('/', '_').str.replace("'", "")
+        '.', '_').str.replace('/', '_').str.replace("'", "").str.replace(":", "_")
 
 
 def load_credentials(api_file):
@@ -41,6 +41,9 @@ def load_bq_from_csv(config_dict, credentials):
     source_blob_name = config_dict['source_blob_name']
     dataset_name = config_dict['dataset_name']
     target_table_name = config_dict['name']
+    del_rows = config_dict['del_rows']
+    del_cols = config_dict['del_cols']
+    logging.debug(config_dict)
 
     source_blob_basename, source_blob_ext = os.path.splitext(source_blob_name)
 
@@ -51,8 +54,26 @@ def load_bq_from_csv(config_dict, credentials):
     source_blob = source_bucket.blob(source_blob_name)
 
     data = source_blob.download_as_string()
-    df = pd.read_csv(StringIO(data.decode('utf-8')), low_memory=False)
+    df = pd.read_csv(StringIO(data.decode('utf-8')), skiprows=del_rows, low_memory=False)
+    #df = df.iloc[3:,1:]
+    print(df)
+    #if len(del_rows) >= 1:
+    #    logging.debug("Delete Rows: " + str(del_rows))
+        # for i in reversed(del_rows):
+
+        # logging.debug("Dropping row: " + str(i))
+        # df.drop(df.index[i], inplace=True)
+    #    df.drop(del_rows, inplace=True)
+    #if len(del_cols) >= 1:
+    #    logging.debug("Delete Cols: " + str(del_cols))
+    #    for j in reversed(del_cols):
+    #        column_numbers = [x for x in range(df.shape[1])]
+    #        column_numbers.remove(j)  # removing column integer index 0
+    #    df = df.iloc[:, column_numbers]
+    df.columns = df.iloc[0]
+    #df = df.reindex(df.index.drop(0)).reset_index(drop=True)
     df.columns = clean_column_headers(df.columns)
+    print(df)
     print("Loading: " + dataset_name + '.' + source_blob_basename)
     df.to_gbq(dataset_name + '.' + target_table_name, project_id=project_id, if_exists='replace')
     print("Finished load of: " + dataset_name + '.' + source_blob_basename)
