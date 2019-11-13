0ba9aa40a19d9761b8f8359ccb08ec37c2d11067
j-buss <jeremy.f.buss@gmail.com>
2019-11-13 05:39:27 -0600

    updated staging load job to create dataset

--- pipeline/python/process/bqLoad/loadBQStaging.py
+++ pipeline/python/process/bqLoad/loadBQStaging.py
@@ -1,5 +1,5 @@
-from google.cloud import bigquery
 from google.cloud import storage
+from google.cloud import bigquery
 from google.oauth2 import service_account
 
 import sys
@@ -10,7 +10,7 @@ import os
 from io import StringIO
 
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
-# logging.disable(logging.CRITICAL)
+logging.disable(logging.CRITICAL)
 
 
 def clean_column_headers(columns):
@@ -34,6 +34,23 @@ def load_credentials(api_file):
     return creds
 
 
+def create_dataset(client, dataset_name):
+    dataset_id = "{}.{}".format(client.project, dataset_name)
+    dataset = bigquery.Dataset(dataset_id)
+    dataset.location = "US"
+
+    try:
+        dataset = client.create_dataset(dataset)
+    except google.api_core.exceptions.Conflict:
+        print("Dataset {}.{} already exists".format(client.project, dataset_name))
+    print("Created dataset {}.{}".format(client.project, dataset_name))
+
+
+def delete_dataset(client, project_id, dataset_name):
+    dataset_id = "{}.{}".format(project_id, dataset_name)
+    client.delete_dataset(dataset_id, delete_contents=True, not_found_ok=True)
+
+
 def load_bq_from_csv(config_dict, credentials):
 
     project_id = config_dict['project_id']
@@ -44,46 +61,39 @@ def load_bq_from_csv(config_dict, credentials):
     del_rows = config_dict['del_rows']
     del_cols = config_dict['del_cols']
     logging.debug(config_dict)
-
     source_blob_basename, source_blob_ext = os.path.splitext(source_blob_name)
 
-    bq_client = bigquery.Client(project=project_id, credentials=credentials)
+    bigquery_client = bigquery.Client(project=project_id, credentials=credentials)
     storage_client = storage.Client(project=project_id, credentials=credentials)
-
     source_bucket = storage_client.get_bucket(bucket_name)
     source_blob = source_bucket.blob(source_blob_name)
+    print("Loading: " + dataset_name + '.' + source_blob_basename)
+
+    create_dataset(bigquery_client, dataset_name)
 
     data = source_blob.download_as_string()
-    df = pd.read_csv(StringIO(data.decode('utf-8')), skiprows=del_rows, low_memory=False)
-    #df = df.iloc[3:,1:]
-    print(df)
-    #if len(del_rows) >= 1:
-    #    logging.debug("Delete Rows: " + str(del_rows))
-        # for i in reversed(del_rows):
+    if len(del_cols) > 0:
+        df = pd.read_csv(StringIO(data.decode('utf-8')), skiprows=del_rows, low_memory=False)
+    else:
+        df = pd.read_csv(StringIO(data.decode('utf-8')), low_memory=False)
 
-        # logging.debug("Dropping row: " + str(i))
-        # df.drop(df.index[i], inplace=True)
-    #    df.drop(del_rows, inplace=True)
-    #if len(del_cols) >= 1:
-    #    logging.debug("Delete Cols: " + str(del_cols))
-    #    for j in reversed(del_cols):
-    #        column_numbers = [x for x in range(df.shape[1])]
-    #        column_numbers.remove(j)  # removing column integer index 0
-    #    df = df.iloc[:, column_numbers]
-    df.columns = df.iloc[0]
-    #df = df.reindex(df.index.drop(0)).reset_index(drop=True)
+    if len(del_cols) > 0:
+        df.drop(df.columns[del_cols], axis=1, inplace=True)
+
     df.columns = clean_column_headers(df.columns)
-    print(df)
-    print("Loading: " + dataset_name + '.' + source_blob_basename)
-    df.to_gbq(dataset_name + '.' + target_table_name, project_id=project_id, if_exists='replace')
+    logging.debug(df.head)
+    df.to_gbq(dataset_name + '.' + target_table_name, project_id=project_id, credentials=credentials, if_exists='replace')
     print("Finished load of: " + dataset_name + '.' + source_blob_basename)
 
 
 if __name__ == "__main__":
 
-    # configuration_file = sys.argv[1]
-    configuration_file = "/home/jeremyfbuss/wi-dpi-analysis/.gcp/bq_load_config.json"
-    credentials = load_credentials("/home/jeremyfbuss/wi-dpi-analysis/.gcp/API_process_data.json")
+    if len(sys.argv) <= 2:
+        print("Usage: python loadBQStaging.py CONFIGURATION_FILE CREDENTIALS_FILE")
+        sys.exit(1)
+    configuration_file = sys.argv[1]
+    credential_file = sys.argv[2]
+    credentials = load_credentials(credential_file)
 
     with open(configuration_file) as config_file:
         data = json.load(config_file)
