a98602c9edda34eb225fc74e0af7264aaaf40864
j-buss <jeremy.f.buss@gmail.com>
2019-11-14 19:23:54 -0600

    updated bqLoad

---
+++ scripts/shell/bqLoad.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+python3 ../../pipeline/python/process/bqLoad/loadBQStaging.py ../../config/2015_bq_load_config.json ../../.gcp/API_process_data.json
+python3 ../../pipeline/python/process/bqLoad/loadBQStaging.py ../../config/2016_bq_load_config.json ../../.gcp/API_process_data.json
+python3 ../../pipeline/python/process/bqLoad/loadBQStaging.py ../../config/2017_bq_load_config.json ../../.gcp/API_process_data.json
+python3 ../../pipeline/python/process/bqLoad/loadBQStaging.py ../../config/2018_bq_load_config.json ../../.gcp/API_process_data.json
+python3 ../../pipeline/python/process/bqLoad/loadBQStaging.py ../../config/2019_bq_load_config.json ../../.gcp/API_process_data.json
