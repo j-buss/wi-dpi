4794e5a53720dcc2a7b287b6db2558b9a7e60469
j-buss <jeremy.f.buss@gmail.com>
2019-11-19 06:06:40 -0600

    working adjInflation.py

--- process/adjInflation.py
+++ process/adjInflation.py
@@ -1,5 +1,6 @@
 import gcp_utilities as gcp
 import pandas as pd
+import cpi
 
 import logging
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
@@ -7,10 +8,12 @@ logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - 
 
 
 if __name__ == "__main__":
+    cpi.update()
     creds = gcp.load_gcp_credentials("../.gcp/API_process_data.json")
     sql = '''
-    select * from `wi-dpi-010.2015.2015_BASE`
+    select * from `wi-dpi-010.2015.2015_BASE` LIMIT 100
     '''
     temp_df = pd.read_gbq(sql, credentials=creds)
-
-    print(temp_df)
+    temp_df['salary_adj'] = temp_df.apply(lambda x: cpi.inflate(x.salary, 2015), axis=1)
+    temp_df['benefits_adj'] = temp_df.apply(lambda x: cpi.inflate(x.benefits, 2015), axis=1)
+    temp_df.to_gbq("2015.2015_BASE_Adjusted",credentials=creds)
