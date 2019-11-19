d01e339ca529dbde54454c91d8d9fcfe49f2345b
j-buss <jeremy.f.buss@gmail.com>
2019-11-19 16:28:56 -0600

    updated adjInflation with config file input

--- process/adjInflation.py
+++ process/adjInflation.py
@@ -1,6 +1,8 @@
 import gcp_utilities as gcp
 import pandas as pd
 import cpi
+import sys
+import json
 
 import logging
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
@@ -10,10 +12,17 @@ logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - 
 if __name__ == "__main__":
     cpi.update()
     creds = gcp.load_gcp_credentials("../.gcp/API_process_data.json")
-    sql = '''
-    select * from `wi-dpi-010.2015.2015_BASE` LIMIT 100
-    '''
-    temp_df = pd.read_gbq(sql, credentials=creds)
-    temp_df['salary_adj'] = temp_df.apply(lambda x: cpi.inflate(x.salary, 2015), axis=1)
-    temp_df['benefits_adj'] = temp_df.apply(lambda x: cpi.inflate(x.benefits, 2015), axis=1)
-    temp_df.to_gbq("2015.2015_BASE_Adjusted",credentials=creds)
+    configuration_file = sys.argv[1]
+
+    with open(configuration_file) as config_file:
+        data = json.load(config_file)
+
+    for json_dict in data:
+        sql = data["SQL"]
+        ref_year = data["Reference Year"]
+        target_dataset = data["Target Dataset"]
+        target_table = data["Target Table"]
+        temp_df = pd.read_gbq(sql, credentials=creds)
+        temp_df['salary_adj'] = temp_df.apply(lambda x: cpi.inflate(x.salary, ref_year), axis=1)
+        temp_df['benefits_adj'] = temp_df.apply(lambda x: cpi.inflate(x.benefits, ref_year), axis=1)
+        temp_df.to_gbq(target_dataset + "." + target_table,credentials=creds)
